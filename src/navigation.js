c500dfd40405163086658c87baf4e2b535a0b238
Berts <romareda@gmail.com>
2024-01-18 22:45:02 +0100

    navegación

--- src/navigation.js
+++ src/navigation.js
@@ -65,6 +65,6 @@ export const footerData = {
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(#)]"></span>
-    © 2024 <a class="text-blue-600 hover:underline dark:text-gray-200" href="/"> RuizLizondo</a> .
+    © 2024 <a class="text-blue-600 hover:underline dark:text-gray-200" href="/">RuizLizondo</a> .
   `,
 };
